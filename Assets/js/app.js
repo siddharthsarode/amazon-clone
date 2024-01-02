117a3d4eb44002b08e2ba1a2290c13122d245c24
siddharthsarode <siddharthsarode279@gmail.com>
2024-01-02 19:52:54 +0530

    Navbar desktop version are completed

---
+++ Assets/js/app.js
@@ -0,0 +1,21 @@
+
+// navbar right side 1st button select language btn
+const languageBtn = document.querySelector("#lang");
+const header = document.querySelector("header");
+
+languageBtn.addEventListener("click", function (event) {
+    event.stopPropagation();
+    header.classList.toggle("language");
+    header.classList.remove("login");
+});
+
+// navbar sign button 
+const signUpBtn = document.querySelector("#signUp");
+
+signUpBtn.addEventListener("click", (event) => {
+    event.stopPropagation();
+    header.classList.toggle("login");
+    header.classList.remove("language");
+})
+
+
